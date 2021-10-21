6ad6eb9433aca9d317613bd7376bd9c9beee3849
Muhammad Anas Seth <m.anasseth00@gmail.com>
2021-10-22 01:41:34 +0500

    Carousel Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './shared/product/product.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { SuccessComponent } from './success/success.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
+    SuccessComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
